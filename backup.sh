4078d77a952be5f80979af9a9fc1d21ad504e813
bopjiang <bopjiang@163.com>
2021-06-13 16:36:31 +0800

    update backup script

--- backup.sh
+++ backup.sh
@@ -6,8 +6,10 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TS=$(date '+%Y%m%d_%H%M%S')
 
-BACKUP_DIR=${SCRIPT_DIR}/backup/conf_repo_backup_${TS}
-BACKUP_FILE=${SCRIPT_DIR}/backup/conf_repo_backup_${TS}.zip
+BACKUP_TMP=${SCRIPT_DIR}/backup/tmp
+BACKUP_DIRNAME=conf_repo_backup_${TS}
+BACKUP_DIR=${BACKUP_TMP}/${BACKUP_DIRNAME}
+BACKUP_FILE=${SCRIPT_DIR}/backup/conf_repo_backup_${TS}.tar.gz
 mkdir -p ${BACKUP_DIR}
 
 cp -r bin ${BACKUP_DIR}/
@@ -15,6 +17,8 @@ cp -r bash ${BACKUP_DIR}/
 
 cd ${SCRIPT_DIR}
 rsync -avr --exclude='path1/to/exclude' --exclude='path2/to/exclude' ./bin ${BACKUP_DIR}/
-rsync -avr --include='bash/custom_bashrc.sh'  ./bash ${BACKUP_DIR}/
-zip -r ${BACKUP_FILE} ${BACKUP_DIR}
+rsync -am --include='bash/custom_bashrc.sh' --exclude='*' ./bash ${BACKUP_DIR}/
+
+cd ${BACKUP_TMP}
+tar -czvf ${BACKUP_FILE} ${BACKUP_DIRNAME}
  
